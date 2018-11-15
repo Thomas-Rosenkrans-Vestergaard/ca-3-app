56368d0a3f5bfd1063a71e05f319103af2ca78b9
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-15 13:49:53 +0100

    basic routes

---
+++ screens/FetchScreen.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class FetchScreen extends React.Component {
+
+    render() {
+        return (
+            <ScrollView>
+                <Text>Fetch</Text>
+            </ScrollView>
+        );
+    }
+}
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class WelcomeScreen extends React.Component {
+
+    render() {
+        return (
+            <ScrollView>
+                <Text>Welcome</Text>
+            </ScrollView>
+        );
+    }
+}
